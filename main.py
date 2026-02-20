763de23c6c28596f6c5700d34aa90a34e6285bea
GarbagCode <romelusfrancy@gmail.com>
2026-02-20 17:09:22 +0000

    reversla signal

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from helpful_functions.aggregateTimeFrames import aggregate_time_frame
 
 df = pd.read_csv("data/btcusd_cleaned.csv")
 
-df.index = pd.to_datetime(df["timestamp"], unit="s", utc="True")
+df["timestamp"] = pd.to_datetime(df["timestamp"], unit="s", utc=True)
+df.set_index("timestamp", inplace=True)
 
-df_30min = aggregate_time_frame(df[29:], 30)
+df_30min = aggregate_time_frame(df, 43800)
 
 print(df_30min.head())
 
-df.to_csv("data/btcusd_30-min_data.csv")
+df.to_csv("data/btcusd_43800-min_data.csv")
